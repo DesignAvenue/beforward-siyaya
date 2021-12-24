ba3a215b0dd4cfe4a49541ba694d86a77475d5ce
Terryo10 <tapiwatererai@outlook.com>
2021-12-24 09:48:35 +0200

    footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -7,9 +7,7 @@
                         <div class="footer-logo">
                             <a href="/"><img src="{{asset('assets/img/logo/logo.png') }}" alt=""></a>
                         </div>
-                        <p class="pr-10">Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse
-                            molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et
-                            accumsan.</p>
+                        <p class="pr-10">Beforward Siyaya Trading - We aid you in purchasing your vehicle and transportation from Beforward </p>
                         <div class="footer-info-wrapper">
                             <div class="f-info">
                                 <span class="icon icon-Pointer"></span>
@@ -17,11 +15,25 @@
                             </div>
                             <div class="f-info">
                                 <span class="icon icon-Phone"></span>
-                                <span class="f-info-text">Phone: +(012) 800 456 789</span>
+                                <span class="f-info-text">Phone: +263772443777
+                                    </span>
+                            </div>
+                            <div class="f-info">
+                                <span class="icon icon-Phone"></span>
+                                <span class="f-info-text">Phone: +263719444777</span>
+                            </div>
+                            <div class="f-info">
+                                <span class="icon icon-Phone"></span>
+                                <span class="f-info-text">Phone: +263772443777</span>
+                            </div>
+                            <div class="f-info">
+                                <span class="icon icon-Phone"></span>
+
+                                <span class="f-info-text">Phone: +263715532060</span>
                             </div>
                             <div class="f-info">
                                 <span class="icon icon-Mail"></span>
-                                <span class="f-info-text">Email: Infor@dazimaxx.com</span>
+                                <span class="f-info-text">Email: info@beforwardzimbabwe.co.zw</span>
                             </div>
                         </div>
                     </div>
@@ -30,42 +42,14 @@
                     <div class="single-footer-widget">
                         <h5 class="pb-35">Quick links</h5>
                         <ul class="footer-list">
-                            <li><a href="about.html">About Us</a></li>
-                            <li><a href="#">Terms &amp; Conditions</a></li>
-                            <li><a href="service.html">Services</a></li>
-                            <li><a href="#">Support</a></li>
-                            <li><a href="Blog-2.html">Blog</a></li>
-                            <li><a href="#">Help &amp; FAQ</a></li>
-                            <li><a href="team.html">Our Team</a></li>
-                            <li><a href="#">Our Forms</a></li>
-                            <li><a href="contact.html">Contact Us</a></li>
-                            <li><a href="#">Privacy Policy</a></li>
-                        </ul>
+                            <li><a href="/about">About Us</a></li>
+
                     </div>
                 </div>
                 <div class="col-lg-4 col-md-6 col-xs-12">
                     <div class="single-footer-widget">
-                        <h5 class="pb-30">Latest tweets</h5>
-                        <div class="single-tweet-text">
-                            <div class="twt-text">
-                                <span><i class="fa fa-twitter"></i></span>
-                                <p>Mirum est notare quam littera gothica <a
-                                        href="https://twitter.com/beforwardzimbabwe">”@ quam nunc putamus parum
-                                        claram”</a> anteposuerit litterarum formas humanitatis.
-                                </p>
-                            </div>
-                            <span class="twt-info">Wednesday / Dec 17 / 2021</span>
-                        </div>
-                        <div class="single-tweet-text">
-                            <div class="twt-text">
-                                <span><i class="fa fa-twitter"></i></span>
-                                <p>Mirum est notare quam littera gothica <a
-                                        href="https://twitter.com/beforwardzimbabwe">”@ quam nunc putamus parum
-                                        claram”</a> anteposuerit litterarum formas humanitatis.
-                                </p>
-                            </div>
-                            <span class="twt-info">Wednesday / Dec 17 / 2021</span>
-                        </div>
+
+
                     </div>
                 </div>
             </div>
@@ -77,20 +61,16 @@
             <div class="row">
                 <div class="col-lg-7 col-xl-6 col-md-8">
                     <div class="footer-links-copyright">
-                        <span>Copyright &copy; 2021 <a href="https://beforwardzimbabwe.co.zw/">beforwardzimbabwe</a>. All Right
+                        <span>Copyright &copy; 2021 <a href="https://designave.co.zw/">DesignAve</a>. All Right
                             Reserved.</span>
                         <div class="social-links">
                             <a href="https://www.facebook.com/beforwardzimbabwe/"><i class="fa fa-facebook"></i></a>
                             <a href="https://twitter.com/beforwardzimbabwe"><i class="fa fa-twitter"></i></a>
-                            <a href="https://www.behance.net/beforwardzimbabwe"><i class="fa fa-behance"></i></a>
-                            <a href="https://dribbble.com/beforwardzimbabwe"><i class="fa fa-dribbble"></i></a>
                         </div>
                     </div>
                 </div>
                 <div class="col-lg-5 col-xl-6 col-md-4">
-                    <div class="payment-img">
-                        <img src="{{asset('assets/img/other/payment.png') }}" alt="">
-                    </div>
+
                 </div>
             </div>
         </div>
